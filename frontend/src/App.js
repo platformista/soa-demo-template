ed7f942d973771abcf01e9bd4be76afdb78c42f4
Vincenzo Russo <v@artetecha.com>
2021-11-16 17:49:00 +0100

    Refactor front-end app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 
 const restUrl = "https://api." + window.location.hostname + "/python";
 
 function App() {
-  const [apiData, setApiData] = useState({});
+    const [apiData, setApiData] = useState({});
 
-  useEffect(() => {
-    getRestWithFetch();
-  }, []);
+    useEffect(() => {
+        getRestWithFetch();
+    }, []);
 
-  const getRestWithFetch = async () => {
-    const response = await fetch(restUrl);
-    const jsonData = await response.json();
-    console.log(jsonData);
-    setApiData(jsonData);
-  };
+    const getRestWithFetch = async () => {
+        const response = await fetch(restUrl);
+        const jsonData = await response.json();
+        console.log(jsonData);
+        setApiData(jsonData);
+    };
 
-   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>How is the world?</h2>
-        <div className="user-container">
-        <h5>Response from python-service through the API gateway https://api.&#123;default&#125;/python :</h5>
-
-        <h2 className="info-item">{apiData.the_world}</h2>
-      </div>
-      </header>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>Alteryx estate status</h2>
+                <div className="user-container">
+                    <ul>
+                        <li>
+                            Assets Service: OK
+                        </li>
+                        <li>
+                            Settings Service: OK
+                        </li>
+                        <li>
+                            Data/Connector Service: OK
+                        </li>
+                        <li>
+                            User Management Service: OK
+                        </li>
+                        <li>
+                            Vault: OK
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
